3835339cf40d3234b471f62dd4e958e47ebbb3e5
ZX <sam.lin.0202www@gmail.com>
2026-05-28 11:46:39 +0800

    整理yolo_detection資料

--- yolo_detection/train.py
+++ yolo_detection/train.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 model = YOLO("yolov8n.pt")
 
 model.train(
-    data=r"C:\Users\samli\Desktop\YOLO\dataset\data.yaml",
+    data=r"C:\Users\samli\Desktop\SAM\GIHUB\ARDUINO_ESP32\yolo_detection\dataset.yaml",
     epochs=10,
     imgsz=640
 )
